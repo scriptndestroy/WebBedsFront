49793a14b6a041f8d140f87942187c0ed6f0efd8
scriptndestroy <ojedamaruny@gmail.com>
2021-06-22 19:37:40 +0200

    🩹 Fix

--- src/config.loc.ts
+++ src/config.loc.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {    
-    webbeds_external:"https://localhost:5001/",    
+    webbeds_external:"https://web-beds-api.herokuapp.com/",    
     DEV:false
 };
